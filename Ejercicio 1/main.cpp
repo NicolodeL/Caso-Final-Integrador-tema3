e7bf15530af014ea4cee1f40f55f016a34a56087
NlodeL <nlodelop@myuax.com>
2023-11-28 11:56:07 +0100

    empiezo creando un directorio y el cmakelist

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <map>
+#include "json11.hpp"
+#include "jsonlib.hpp"
+
+int main() {
+ std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
